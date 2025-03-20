fcae1200b94277a3cd41d1d3928d8cbee8c1b237
rafael mafort <rafaelmafortc@gmail.com>
2025-03-20 17:00:59 -0300

    feat: modeToggle in sign-in page

--- src/app/(public)/sign-in/page.tsx
+++ src/app/(public)/sign-in/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from '@/components/theme-switcher';
+
 export default function SignIn() {
     return (
         <main>
             <div>Sign In</div>
+            <ModeToggle />
         </main>
     );
 }
