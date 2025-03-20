2ef6bd1edb432c8f9125e88d77c412a449e1654d
rafael mafort <rafaelmafortc@gmail.com>
2025-03-20 20:19:30 -0300

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+
+export default function NotFound() {
+    const router = useRouter();
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-black gap-4">
+            <h1 className="text-2xl p-2 font-bold text-white">
+                404 | Page not found
+            </h1>
+            <Button
+                variant={'outline'}
+                size={'lg'}
+                onClick={() => router.back()}
+            >
+                Voltar
+            </Button>
+        </div>
+    );
+}
