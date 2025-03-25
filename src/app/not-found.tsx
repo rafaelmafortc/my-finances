97c800c3b2f03c056f5428e66857da10e449cd43
rafael mafort <rafaelmafortc@gmail.com>
2025-03-25 18:36:13 -0300

    fix: notFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,12 @@
 'use client';
 
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-
-import { Button } from '@/components/ui/button';
 
 export default function NotFound() {
     const router = useRouter();
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-black gap-4">
-            <h1 className="text-2xl p-2 font-bold text-white">
-                404 | Page not found
-            </h1>
-            <Button
-                variant={'outline'}
-                size={'lg'}
-                onClick={() => router.back()}
-            >
-                Voltar
-            </Button>
-        </div>
-    );
+    useEffect(() => {
+        router.push('/');
+    }, [router]);
 }
