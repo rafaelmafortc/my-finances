221eb1364644cf11677261f8d5685ff1f003f529
rafael mafort <rafaelmafortc@gmail.com>
2025-03-27 22:40:58 -0300

    fix: PrivateLayout

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
+import { Loader2 } from 'lucide-react';
+
 import { Navbar } from '@/components/navbar';
 import { useAuth } from '@/provider/auth-provider';
-import { Loader, Loader2 } from 'lucide-react';
 
 export default function PrivateLayout({
     children,
@@ -10,13 +12,12 @@ export default function PrivateLayout({
     children: React.ReactNode;
 }) {
     const { user, loading } = useAuth();
+    const t = useTranslations('login');
 
     if (loading) {
         return (
-            <div className="flex items-center justify-center h-screen">
-                <div className="flex flex-col items-center gap-2 text-foreground">
-                    <Loader2 className="h-8 w-8 animate-spin" />
-                </div>
+            <div className="flex h-screen w-screen items-center justify-center">
+                <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
             </div>
         );
     }
@@ -25,11 +26,9 @@ export default function PrivateLayout({
         <div>
             <Navbar />
             <div className="sm:ml-16 p-4">
-                {user?.displayName && (
-                    <div className="mb-4 text-lg">
-                        Bem-vindo, {user.displayName}
-                    </div>
-                )}
+                <div className="mb-4 text-sm text-muted-foreground">
+                    {t('welcome')}, {user?.displayName}
+                </div>
                 {children}
             </div>
         </div>
