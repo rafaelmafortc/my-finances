dca0137fb01c9bc76a9927202378c4c3784d2dc0
rafael mafort <rafaelmafortc@gmail.com>
2025-03-28 21:34:18 -0300

    fix yarn build

--- src/app/(private)/expense/page.tsx
+++ src/app/(private)/expense/page.tsx
@@ -1,7 +1,7 @@
 import { useTranslations } from 'next-intl';
 
 import PageLayout from '@/components/layouts/page-layout';
-import { PieChartComponent } from '@/components/pie-chart';
+import PieChart from '@/components/pie-chart';
 
 export default function Expense() {
     const t = useTranslations('navbar');
@@ -9,7 +9,7 @@ export default function Expense() {
     return (
         <main className="flex-1 flex flex-col">
             <PageLayout title={t('expense')}>
-                <PieChartComponent />
+                <PieChart />
             </PageLayout>
         </main>
     );
--- src/app/(private)/income/page.tsx
+++ src/app/(private)/income/page.tsx
@@ -1,7 +1,7 @@
 import { useTranslations } from 'next-intl';
 
 import PageLayout from '@/components/layouts/page-layout';
-import { PieChartComponent } from '@/components/pie-chart';
+import PieChart from '@/components/pie-chart';
 
 export default function Income() {
     const t = useTranslations('navbar');
@@ -9,7 +9,7 @@ export default function Income() {
     return (
         <main className="flex-1 flex flex-col">
             <PageLayout title={t('income')}>
-                <PieChartComponent />
+                <PieChart />
             </PageLayout>
         </main>
     );
