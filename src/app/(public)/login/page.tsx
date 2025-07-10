22ef6ea85998814bf7f04aa60f2f345da0594345
rafaelmafortc <rafaelmafortc@gmail.com>
2025-07-10 18:03:53 -0300

    feat: login page

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,22 @@
+import { PiggyBank } from 'lucide-react';
+
+import { LoginForm } from '@/components/login-form';
+
+export default function Login() {
+    return (
+        <div className="bg-secondary flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <div className="flex w-full max-w-sm flex-col gap-6">
+                <a
+                    href="#"
+                    className="flex items-center gap-2 self-center font-medium"
+                >
+                    <div className="bg-primary-foreground text-primary flex size-7 items-center justify-center rounded-md">
+                        <PiggyBank className="size-6" />
+                    </div>
+                    MyFinances
+                </a>
+                <LoginForm />
+            </div>
+        </div>
+    );
+}
