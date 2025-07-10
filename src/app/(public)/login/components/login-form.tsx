f8e803f03a8758173c0bb43d6e26f8bd17da7545
rafaelmafortc <rafaelmafortc@gmail.com>
2025-07-10 19:06:15 -0300

    fix: components pages

--- src/app/(public)/login/components/login-form.tsx
+++ src/app/(public)/login/components/login-form.tsx
@@ -1,12 +1,12 @@
 import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/button';
 import {
     Card,
     CardContent,
     CardDescription,
     CardHeader,
     CardTitle,
-} from '@/components/ui/card';
+} from '@/components/card';
 
 export function LoginForm({
     className,
--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,6 +1,6 @@
 import { PiggyBank } from 'lucide-react';
 
-import { LoginForm } from '@/components/login-form';
+import { LoginForm } from './components/login-form';
 
 export default function Login() {
     return (
