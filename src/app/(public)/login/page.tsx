8d666e722afb791af0c070be49348b7357fddfaf
rafaelmafortc <rafaelmafortc@gmail.com>
2025-07-10 18:58:41 -0300

    fix: login return to landingpage

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -6,12 +6,15 @@ export default function Login() {
     return (
         <div className="bg-secondary flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
-                <span className="flex items-center gap-2 self-center font-medium">
+                <a
+                    href="/"
+                    className="flex items-center gap-2 self-center font-medium"
+                >
                     <div className="bg-primary-foreground text-primary flex size-7 items-center justify-center rounded-md">
                         <PiggyBank className="size-6" />
                     </div>
                     <h1>MyFinances</h1>
-                </span>
+                </a>
                 <LoginForm />
             </div>
         </div>
