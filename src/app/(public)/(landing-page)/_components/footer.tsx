f56a6669d5e16724d14fa438cddc0f2536689406
rafaelmafortc <rafaelmafortc@gmail.com>
2025-07-11 18:42:37 -0300

    fix: padding

--- src/app/(public)/(landing-page)/_components/footer.tsx
+++ src/app/(public)/(landing-page)/_components/footer.tsx
@@ -1,8 +1,13 @@
 export function Footer() {
     return (
-        <div className="flex justify-between items-center pt-8 border-t-2">
-            <h1 className="text-2xl font-semibold">MyFinances</h1>
-            <span>© Rafael Mafort Coimbra</span>
+        <div className="flex flex-col justify-between items-center pt-8 border-t-2 gap-4">
+            <div className="flex w-full justify-between items-center">
+                <h1 className="text-2xl font-semibold">MyFinances</h1>
+                <span>Right Footer</span>
+            </div>
+            <span className="text-xs text-muted-foreground">
+                powered by Rafael Mafort Coimbra
+            </span>
         </div>
     );
 }
--- src/app/(public)/(landing-page)/page.tsx
+++ src/app/(public)/(landing-page)/page.tsx
@@ -4,7 +4,7 @@ import { Header } from './_components/header';
 
 export default function LandingPage() {
     return (
-        <main className="bg-primary p-8">
+        <main className="bg-primary p-2 sm:p-4">
             <Header />
             <Main />
             <Footer />
