de9cc38ab28f63a15c558438b87d53f98fd1dc0f
rafaelmafortc <rafaelmafortc@gmail.com>
2025-07-11 19:20:53 -0300

    feat: empty categories

--- src/app/(private)/categories/page.tsx
+++ src/app/(private)/categories/page.tsx
@@ -1,10 +1,23 @@
 import PrivatePageLayout from '@/components/page-layout';
+import { Button } from '@/components/ui/button';
 
 export default function Categories() {
     return (
         <PrivatePageLayout title="Categorias">
-            <div className="w-full h-full border-2 border-dashed rounded-lg p-2 border-accent">
-                Categories
+            <div className="flex items-center justify-center min-h-full border-2 border-dashed rounded-lg p-4 border-accent">
+                <div className="w-full max-w-md ">
+                    <div className="flex flex-col items-center gap-4">
+                        <div className="flex flex-col items-center">
+                            <p className="text-xl">
+                                Nenhuma categoria cadastrada
+                            </p>
+                            <p className="text-base text-muted-foreground">
+                                Adicione sua primeira categoria para começar
+                            </p>
+                        </div>
+                        <Button color="cian">Adicionar Categoria</Button>
+                    </div>
+                </div>
             </div>
         </PrivatePageLayout>
     );
