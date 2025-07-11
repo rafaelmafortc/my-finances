b852b78437e9d7e31f9574e73a8e89824c4e5a20
rafaelmafortc <rafaelmafortc@gmail.com>
2025-07-11 00:06:09 -0300

    remove link logo

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,11 +1,19 @@
+import { Link, PiggyBank } from 'lucide-react';
 import { LoginForm } from './_components/login-form';
-import { LinkLogo } from '@/components/link-logo';
 
 export default function Login() {
     return (
         <div className="bg-primary flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6 items-center">
-                <LinkLogo />
+                <Link
+                    href="/"
+                    className="flex items-center p-2 gap-2 font-medium"
+                >
+                    <div className="bg-primary-foreground text-primary flex size-8 items-center justify-center rounded-md">
+                        <PiggyBank className="size-6" />
+                    </div>
+                    <h1>MyFinances</h1>
+                </Link>
                 <LoginForm />
             </div>
         </div>
