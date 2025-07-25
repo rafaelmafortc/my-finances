fd0db84597c77a3543e4f6183274c5c01a48a851
rafaelmafortc <rafaelmafortc@gmail.com>
2025-07-25 17:13:56 -0300

    feat: founder page

---
+++ src/app/(public)/(landing-page)/_components/founder.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+
+import founderImage from 'public/founder-image.jpg';
+
+export function Founder() {
+    return (
+        <div className="flex flex-col justify-center md:flex-row gap-8 md:gap-16 items-center p-8 md:p-16">
+            <Image
+                width={280}
+                height={280}
+                src={founderImage}
+                alt="Founder"
+                className="rounded-full object-cover w-40 h-40 md:w-72 md:h-72"
+            />
+            <div className="flex flex-col gap-4 text-center md:text-left md:max-w-1/2">
+                <span className="font-bold text-xl md:text-2xl">
+                    Olá, sou o Rafael, o único fundador do MyFinances
+                </span>
+                <span className="text-sm md:text-base text-muted-foreground">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Unde, nesciunt dignissimos, sapiente eum perferendis
+                    pariatur repellat nobis consequatur labore doloremque
+                    tempora. Voluptatum, suscipit nemo facilis delectus ipsam
+                    eveniet neque pariatur!
+                </span>
+            </div>
+        </div>
+    );
+}
--- src/app/(public)/(landing-page)/page.tsx
+++ src/app/(public)/(landing-page)/page.tsx
@@ -1,12 +1,14 @@
-import { Footer } from './_components/footer';
-import { Main } from './_components/main';
 import { Header } from './_components/header';
+import { Main } from './_components/main';
+import { Founder } from './_components/founder';
+import { Footer } from './_components/footer';
 
 export default function LandingPage() {
     return (
         <main className="bg-primary p-2 sm:p-6">
             <Header />
             <Main />
+            <Founder />
             <Footer />
         </main>
     );
