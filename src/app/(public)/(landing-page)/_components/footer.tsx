b79b19d9531ec82631a4e4cc671043050c6d9086
rafaelmafortc <rafaelmafortc@gmail.com>
2025-07-25 17:17:38 -0300

    fix: responsive footer

--- src/app/(public)/(landing-page)/_components/footer.tsx
+++ src/app/(public)/(landing-page)/_components/footer.tsx
@@ -4,7 +4,7 @@ import { Input } from '@/components/ui/input';
 export function Footer() {
     return (
         <div className="flex flex-col justify-between items-center pt-8 border-t-2 gap-4">
-            <div className="flex w-full justify-between items-center">
+            <div className="flex md:flex-row flex-col w-full justify-between md:items-center gap-4">
                 <h1 className="text-2xl font-semibold">MyFinances</h1>
                 <div className="flex flex-col gap-4">
                     <span>Atualizações direto na sua caixa de email</span>
