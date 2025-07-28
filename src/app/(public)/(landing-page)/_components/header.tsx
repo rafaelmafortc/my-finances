1ff89b0db82b8069c5e51089411cd8a212e06b3b
rafaelmafortc <rafaelmafortc@gmail.com>
2025-07-27 22:41:15 -0300

    feat: prefetch on login button

--- src/app/(public)/(landing-page)/_components/header.tsx
+++ src/app/(public)/(landing-page)/_components/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
     return (
         <div className="flex justify-between items-center">
             <h1 className="text-2xl font-semibold">MyFinances</h1>
-            <Link href="/login">
+            <Link prefetch href="/login">
                 <Button color="lime" className="w-32">
                     Entrar
                 </Button>
