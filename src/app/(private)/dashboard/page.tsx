9c8d115210e4aafb114effcf092aeb9ebb64f06d
rafaelmafortc <rafaelmafortc@gmail.com>
2025-08-04 11:50:43 -0300

    feat: calendar filter on dashboard page

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import PrivatePageLayout from '@/components/private-page-layout';
 
 export default function Dashboard() {
     return (
-        <PrivatePageLayout title="Dashboard">
+        <PrivatePageLayout title="Dashboard" hasCalendarFilter>
             <div>Dashboard</div>
         </PrivatePageLayout>
     );
--- src/components/private-page-layout.tsx
+++ src/components/private-page-layout.tsx
@@ -14,15 +14,17 @@ import {
 export default function PrivatePageLayout({
     children,
     title,
+    hasCalendarFilter = false,
 }: Readonly<{
     children: React.ReactNode;
     title: string;
+    hasCalendarFilter?: boolean;
 }>) {
     return (
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <header className="bg-sidebar border-b-2 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                <header className="bg-sidebar border-b-2 flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1" />
                         <Separator
@@ -35,6 +37,9 @@ export default function PrivatePageLayout({
                             </BreadcrumbItem>
                         </Breadcrumb>
                     </div>
+                    {hasCalendarFilter && (
+                        <div className="px-4">CALENDARIO</div>
+                    )}
                 </header>
                 <div className="h-full w-full p-4">{children}</div>
             </SidebarInset>
