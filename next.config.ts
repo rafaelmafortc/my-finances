7b31c8d0506b5a2cda9cce91cf4234900695743f
rafaelmafortc <rafaelmafortc@gmail.com>
2025-08-07 17:32:34 -0300

    refactor: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com'], // autoriza imagens do login Google
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+        ],
     },
 };
 
