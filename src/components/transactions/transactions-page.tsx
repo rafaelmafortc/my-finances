9c1a2897bde63166f498ade115be72d9db792879
rafaelmafortc <rafaelmafortc@gmail.com>
2025-08-08 12:13:10 -0300

    feat: transactions page isLoading

--- src/components/transactions/transactions-page.tsx
+++ src/components/transactions/transactions-page.tsx
@@ -1,13 +1,19 @@
 'use client';
 
+import { Loader2 } from 'lucide-react';
+
 import NoDataTransactionsPage from '@/components/transactions/no-data-transactions-page';
 import { TransactionsTable } from '@/components/transactions/transactions-table';
 import { useTransactions } from '@/hooks/use-transactions';
 
 export default function TransactionsPageComponent() {
-    const { transactions } = useTransactions();
+    const { transactions, isLoading } = useTransactions();
 
-    return transactions.length ? (
+    return isLoading ? (
+        <div className="flex items-center justify-center w-full h-screen">
+            <Loader2 className="h-8 w-8 animate-spin" />
+        </div>
+    ) : transactions.length ? (
         <TransactionsTable />
     ) : (
         <NoDataTransactionsPage />
