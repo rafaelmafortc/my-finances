dfb6bb2910f059e3a6c83b7ae29bb26ca7fd711f
rafaelmafortc <rafaelmafortc@gmail.com>
2025-08-11 11:24:34 -0300

    refactor: no data transactions

--- src/components/transactions/transactions-page.tsx
+++ src/components/transactions/transactions-page.tsx
@@ -1,14 +1,19 @@
 'use client';
 
+import { format, parse } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { Loader2 } from 'lucide-react';
 
-import NoDataTransactionsPage from '@/components/transactions/no-data-transactions-page';
+import { TransactionDialog } from '@/components/transactions/transaction-dialog';
 import { TransactionsTable } from '@/components/transactions/transactions-table';
 import { useTransactions } from '@/hooks/use-transactions';
 import { useSelectedDate } from '@/providers/selected-date-provider';
 
 export default function TransactionsPage() {
     const { month } = useSelectedDate();
+    const dateObj = month ? parse(month, 'yyyy-MM', new Date()) : new Date();
+
+    const formattedMonth = format(dateObj, "MMMM 'de' yyyy", { locale: ptBR });
 
     const { transactions, isLoading } = useTransactions(month);
 
@@ -19,6 +24,18 @@ export default function TransactionsPage() {
     ) : transactions.length ? (
         <TransactionsTable />
     ) : (
-        <NoDataTransactionsPage />
+        <div className="flex items-center justify-center min-h-full border-2 border-dashed rounded-lg p-4 border-accent">
+            <div className="w-full max-w-md ">
+                <div className="flex flex-col items-center gap-4">
+                    <div className="flex flex-col items-center text-center">
+                        <p className="text-xl lg:text-nowrap">{`Nenhuma transação cadastrada para o mês ${formattedMonth}`}</p>
+                        <p className="text-base text-muted-foreground">
+                            Adicione sua primeira transação para começar
+                        </p>
+                    </div>
+                    <TransactionDialog />
+                </div>
+            </div>
+        </div>
     );
 }
