2539f7a26a81278ce661b103e504be5c06e13d8e
rafaelmafortc <rafaelmafortc@gmail.com>
2025-08-15 17:38:52 -0300

    feat: validation on transaction table pagination

--- src/components/transactions/transactions-table.tsx
+++ src/components/transactions/transactions-table.tsx
@@ -337,74 +337,82 @@ export function TransactionsTable() {
                         )}
                     </TableBody>
                 </Table>
-                <div className="flex items-center justify-end p-2 bg-accent">
-                    <div className="flex w-full items-center gap-8 lg:w-fit">
-                        <div className="hidden items-center gap-2 lg:flex">
-                            <Label
-                                htmlFor="rows-per-page"
-                                className="text-sm font-medium"
-                            >
-                                Transações por página
-                            </Label>
-                            <Select
-                                value={`${table.getState().pagination.pageSize}`}
-                                onValueChange={(value) => {
-                                    table.setPageSize(Number(value));
-                                }}
-                            >
-                                <SelectTrigger
-                                    size="sm"
-                                    className="w-20"
-                                    id="rows-per-page"
+                {transactions.length > 10 && (
+                    <div className="flex items-center justify-end p-2 bg-accent">
+                        <div className="flex w-full items-center gap-8 lg:w-fit">
+                            <div className="hidden items-center gap-2 lg:flex">
+                                <Label
+                                    htmlFor="rows-per-page"
+                                    className="text-sm font-medium"
                                 >
-                                    <SelectValue
-                                        placeholder={
-                                            table.getState().pagination.pageSize
-                                        }
-                                    />
-                                </SelectTrigger>
-                                <SelectContent side="top">
-                                    {[10, 20, 30, 40, 50].map((pageSize) => (
-                                        <SelectItem
-                                            key={pageSize}
-                                            value={`${pageSize}`}
-                                        >
-                                            {pageSize}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                        </div>
-                        <div className="flex w-fit items-center justify-center text-sm font-medium">
-                            Página {table.getState().pagination.pageIndex + 1}{' '}
-                            de {table.getPageCount()}
-                        </div>
-                        <div className="ml-auto flex items-center gap-2 lg:ml-0">
-                            <Button
-                                variant="outline"
-                                className="size-8"
-                                size="icon"
-                                onClick={() => table.previousPage()}
-                                disabled={!table.getCanPreviousPage()}
-                            >
-                                <span className="sr-only">
-                                    Go to previous page
-                                </span>
-                                <ChevronLeft />
-                            </Button>
-                            <Button
-                                variant="outline"
-                                className="size-8"
-                                size="icon"
-                                onClick={() => table.nextPage()}
-                                disabled={!table.getCanNextPage()}
-                            >
-                                <span className="sr-only">Go to next page</span>
-                                <ChevronRight />
-                            </Button>
+                                    Transações por página
+                                </Label>
+                                <Select
+                                    value={`${table.getState().pagination.pageSize}`}
+                                    onValueChange={(value) => {
+                                        table.setPageSize(Number(value));
+                                    }}
+                                >
+                                    <SelectTrigger
+                                        size="sm"
+                                        className="w-20"
+                                        id="rows-per-page"
+                                    >
+                                        <SelectValue
+                                            placeholder={
+                                                table.getState().pagination
+                                                    .pageSize
+                                            }
+                                        />
+                                    </SelectTrigger>
+                                    <SelectContent side="top">
+                                        {[10, 20, 30, 40, 50].map(
+                                            (pageSize) => (
+                                                <SelectItem
+                                                    key={pageSize}
+                                                    value={`${pageSize}`}
+                                                >
+                                                    {pageSize}
+                                                </SelectItem>
+                                            )
+                                        )}
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div className="flex w-fit items-center justify-center text-sm font-medium">
+                                Página{' '}
+                                {table.getState().pagination.pageIndex + 1} de{' '}
+                                {table.getPageCount()}
+                            </div>
+                            <div className="ml-auto flex items-center gap-2 lg:ml-0">
+                                <Button
+                                    variant="outline"
+                                    className="size-8"
+                                    size="icon"
+                                    onClick={() => table.previousPage()}
+                                    disabled={!table.getCanPreviousPage()}
+                                >
+                                    <span className="sr-only">
+                                        Go to previous page
+                                    </span>
+                                    <ChevronLeft />
+                                </Button>
+                                <Button
+                                    variant="outline"
+                                    className="size-8"
+                                    size="icon"
+                                    onClick={() => table.nextPage()}
+                                    disabled={!table.getCanNextPage()}
+                                >
+                                    <span className="sr-only">
+                                        Go to next page
+                                    </span>
+                                    <ChevronRight />
+                                </Button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     );
