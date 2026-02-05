4f6ea100a3944b9ec62d23135d90005ee1ce433d
rafaelmafortc <rafaelmafortc@gmail.com>
2026-02-05 12:00:29 -0300

    feat: links to pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { PiggyBank } from 'lucide-react';
 
 import { LoginForm } from '@/modules/auth';
@@ -7,7 +9,9 @@ export default async function Page() {
     <main className="min-h-screen flex flex-col bg-primary-foreground bg-size-[24px_24px] ">
       <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="flex items-center gap-2 ">
-          <PiggyBank className="size-6" />
+          <Link href="/">
+            <PiggyBank className="size-6" />
+          </Link>
           <span className="text-xl">MyFinances</span>
         </div>
         <div className="flex w-full max-w-sm flex-col gap-6 items-center">
--- src/app/(landingpage)/page.tsx
+++ src/app/(landingpage)/page.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
 export default function LandingPage() {
   return (
     <div>
       <h1>Landing Page</h1>
+      <Link
+        href="/login"
+        className={cn(buttonVariants({ variant: 'default' }))}
+      >
+        Login
+      </Link>
     </div>
   );
 }
