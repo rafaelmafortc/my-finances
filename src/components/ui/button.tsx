390fea12a5311c0cb244af7cb83e22ef3f322216
rafaelmafortc <rafaelmafortc@gmail.com>
2026-02-10 11:52:30 -0300

    fix: suppressHydrationWarning in button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -61,6 +61,7 @@ function Button({
       data-variant={variant}
       data-size={size}
       className={cn(buttonVariants({ variant, size, className }))}
+      suppressHydrationWarning
       {...props}
     >
       {isLoading ? (
